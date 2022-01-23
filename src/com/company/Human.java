e82b2daf124c81e8e572369f85a16c5b57c57206
user <dawidgrabowicz12@onet.pl>
2022-01-23 13:59:58 +0100

    Zadanie 3 i 4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,4 +1,6 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Human {
     public String firstName;
@@ -6,11 +8,14 @@ public class Human {
     public Integer age;
     public Animal pet;
     public Car car;
+    public Double salary;
+
 
     public Human(String firstName, String lastName, Integer age){
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.salary = 0.0;
     }
 
     public void setPet(Animal pet) {
@@ -19,5 +24,20 @@ public class Human {
 
     public Animal getPet() {
         return pet;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        if(salary <= 0){
+            System.out.println("Nie sądze, nie bede robil za darmo, a tym bardziej doplacal");
+        }else{
+            System.out.println("Nowe dane zostaly wyslane do systemu ksiegowego");
+            System.out.println("Proszę odebrac aneks w dziale kadr");
+            System.out.println("ZUS i US został poinformowany o zmianie wypłaty");
+            this.salary = salary;
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,18 @@ public class Main {
     public static void main(String[] args) {
         Human student = new Human("Dawid", "Grabowicz", 25);
         Human teacher = new Human("Zbigniew", "Boniek", 25);
-        Car opel = new Car("Astra", "Opel", "Red", 1999);
-        Car volvo = new Car("XC60", "Volvo", "Black", 2019);
-        student.car = opel;
-        teacher.car = volvo;
-
-        System.out.println("Imie: " + student.firstName);
-        System.out.println("Nazwisko: " + student.lastName);
-        System.out.println("Wiek: " + student.age);
-        System.out.println("Model smaochodu: " + student.car.model);
-        System.out.println("Producent smaochodu: " + student.car.producer);
-        System.out.println("Kolor smaochodu: " + student.car.colour);
-        System.out.println("Rok produkcji smaochodu: " + student.car.yearOfProduction);
-        System.out.println("-------------------------------------------------------");
-        System.out.println("Imie: " + teacher.firstName);
-        System.out.println("Nazwisko: " + teacher.lastName);
-        System.out.println("Wiek: " + teacher.age);
-        System.out.println("Model smaochodu: " + teacher.car.model);
-        System.out.println("Producent smaochodu: " + teacher.car.producer);
-        System.out.println("Kolor smaochodu: " + teacher.car.colour);
-        System.out.println("Rok produkcji smaochodu: " + teacher.car.yearOfProduction);
+        System.out.println("---------------------------------------------");
+        student.setSalary(0.0);
+        System.out.println("---------------------------------------------");
+        System.out.println(student.getSalary());
+        System.out.println("---------------------------------------------");
+        student.setSalary(-1.0);
+        System.out.println("---------------------------------------------");
+        System.out.println(student.getSalary());
+        System.out.println("---------------------------------------------");
+        student.setSalary(100.0);
+        System.out.println("---------------------------------------------");
+        System.out.println(student.getSalary());
 
     }
 }
