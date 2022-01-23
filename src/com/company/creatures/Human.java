ca2665d5ec4da85c70b8beade106613364024a5f
user <dawidgrabowicz12@onet.pl>
2022-01-23 20:33:24 +0100

    Zadanie 11

--- src/com/company/creatures/Human.java
+++ src/com/company/creatures/Human.java
@@ -2,12 +2,15 @@ package com.company.creatures;
 import com.company.devices.Car;
 import com.company.devices.Phone;
 
+import java.util.Arrays;
+
 public class Human extends Animal{
+    private static final int DEFAULT_GARAGE_SIZE = 10;
     public String firstName;
     public String lastName;
     public Integer age;
     public Animal pet;
-    public Car car;
+    public Car[] garage;
     public Phone mobile;
     public Double salary;
     public Double cash;
@@ -20,6 +23,7 @@ public class Human extends Animal{
         this.age = 0;
         this.salary = 0.0;
         this.cash = 0.0;
+        this.garage = new Car[DEFAULT_GARAGE_SIZE];
     }
 
     public void setPet(Animal pet) {
@@ -45,17 +49,25 @@ public class Human extends Animal{
         }
     }
 
-    public Car getCar() {
-        return car;
+    public Car getCar(Integer space) {
+        return garage[space];
     }
 
-    public void setCar(Car car) {
+    public void setCar(Car car, Integer space) {
         if(salary > car.value){
-            this.car = car;
-            System.out.println("Udało sie kupić auto za gotówkę!");
+            if(garage[space] != null){
+                this.garage[space] = car;
+                System.out.println("Udało sie kupić auto za gotówkę!");
+            }else{
+                System.out.println("To miejsce w garazu jest juz zajete przez inne auto");
+            }
         }else if(salary > (car.value / 12)){
-            this.car = car;
-            System.out.println("Udało się kupić auto na kredyt!");
+            if(garage[space] != null){
+                this.garage[space] = car;
+                System.out.println("Udało się kupić auto na kredyt!");
+            }else{
+                System.out.println("To miejsce w garazu jest juz zajete przez inne auto");
+            }
         }else{
             System.out.println("Nie udało się kupić auta, za mało zarabiasz");
         }
@@ -64,4 +76,62 @@ public class Human extends Animal{
     public String toString(){
         return "Imie: " + firstName + " Nazwisko: " + lastName + " Wiek: " + age;
     }
+
+    public boolean hasCar(Car newCar) {
+        for (Car car : this.garage) {
+            if (car != null && car.equals(newCar)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasFreeSpace() {
+        for (Car car : this.garage) {
+            if (car != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void removeCar(Car car) {
+        for(int i = 0; i < this.garage.length; i++){
+            if(this.garage[i] == car){
+                this.garage[i] = null;
+                i = this.garage.length;
+                System.out.println("Usunieto samochod " + car + " z garazu");
+            }else{
+                System.out.println("Nie masz takiego auta w garazu");
+            }
+        }
+    }
+
+    public void addCar(Car car) {
+        Integer space = 0;
+        for (int i = 0; i < this.garage.length; i++) {
+            if (this.garage[i] == null) {
+                this.garage[i] = car;
+                System.out.println("Dodano auto do garazu w miejsce nr: " + i);
+                i = this.garage.length;
+            }else{
+                if(i == this.garage.length - 1){
+                    System.out.println("Nie masz miejsca w garazu");
+                }
+            }
+        }
+
+    }
+
+    public Double getGarageValue(){
+        Double totalValue = 0.0;
+        for (Car car : this.garage) {
+            if (car != null) {
+                totalValue += car.value;
+            }
+        }
+        System.out.println("Łączna wartość samochodow w garazu:");
+        return totalValue;
+    }
+
 }
